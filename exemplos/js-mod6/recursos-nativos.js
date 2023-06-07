d2ee3cf9ab1a0ca0460c6d5cd3307eb073c7c129
Pablo <125980185+PabloDevFE@users.noreply.github.com>
2023-06-06 21:12:56 -0300

    exercicios

---
+++ exemplos/js-mod6/recursos-nativos.js
@@ -0,0 +1,4 @@
+console.log("Seja bem-vindo!")
+alert("Alerta")
+confirm("Faça uma escolha")
+prompt("Qual o seu nome?")
---
+++ exemplos/js-mod6/tipos-primitivos.js
@@ -0,0 +1,5 @@
+Number //Todos os números estão incluidos (1, 4, 5.3, 1.222...)
+String //Qualquer coisa usada entre ("" ou '' ou ``), mantém o formato de texto (perde todas as outras propriedades)
+Boolean //Falso ou Verdadeiro
+null //NADA (diferente de 0)
+undefined //valor indefinido
