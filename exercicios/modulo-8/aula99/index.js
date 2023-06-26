c638f11215181297704b5b0aee32737d9191ace8
Pablo Henrique <125980185+PabloDevFE@users.noreply.github.com>
2023-06-26 19:15:12 -0300

    fazendo exercicio da aula 100
    um codigo que inverte qualquer palavra digitada, mas cuidado com a letra "e"... rs

--- exercicios/modulo-8/aula99/index.js
+++ exercicios/modulo-8/aula99/index.js
@@ -2,8 +2,15 @@ let spaceshipName = prompt("Qual é o nome da espaçonave?")
 let spaceshipNewName = ""
 
 maxQuant = spaceshipName.length
-console.log(maxQuant)
-for (let i = Number(maxQuant); i > spaceshipName; i--) {
-    console.log(spaceshipName[i])
+
+for (let i = Number(maxQuant-1); i >= 0; i--) {
+    if (spaceshipName[i] != "e") {
+        spaceshipNewName += spaceshipName[i] 
+    } else {
+        alert("[ERR0R] CARACTERE SENSÍVEL: E \nINVERSÃO MÁ SUCEDIDA")
+        break
+    }
 }
 
+alert(`Nome original da nave: ${spaceshipName} \nNome após a inversão: ${spaceshipNewName}`)
+console.log(spaceshipNewName)
